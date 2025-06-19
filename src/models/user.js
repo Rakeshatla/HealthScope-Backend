f68ebfbcf05e99fb12297c81075083901e27f09c
Rakeshatla <151203741+Rakeshatla@users.noreply.github.com>
2025-06-19 23:45:25 +0530

    feat:user model created

---
+++ src/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String
+    },
+    LastName: {
+        type: String
+    },
+    age: {
+        type: Number
+    },
+    gender: {
+        type: String
+    }
+})
+module.exports = mongoose.model('User', userSchema);
